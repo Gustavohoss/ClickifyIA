20c0f6b16c450618dbb609e427fdea6516526446
Gustavo Henrique <ghkueigamer120@gmail.com>
2025-12-19 08:54:58 +0000

    Deixa mais forte o roxo em volta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function LandingPage() {
                   </motion.div>
                 </GradientButton>
               </Link>
-              <div className="shadow-[0_0_30px_5px_rgba(192,132,252,0.15)] rounded-2xl w-full">
+              <div className="shadow-[0_0_40px_10px_rgba(192,132,252,0.3)] rounded-2xl w-full">
                 <Safari_01 />
               </div>
             </motion.div>
