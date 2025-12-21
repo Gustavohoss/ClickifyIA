1b8696894bfcfce22109c5d2a3d550197ccbdfe5
Gustavo Henrique <ghkueigamer120@gmail.com>
2025-12-21 22:21:04 +0000

    Try fixing this error: `Runtime ReferenceError: ArrowRight is not define

--- src/app/prompt-builder/page.tsx
+++ src/app/prompt-builder/page.tsx
@@ -29,6 +29,7 @@ import {
   ArrowLeft,
   PencilRuler,
   Package,
+  ArrowRight,
 } from 'lucide-react';
 import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
